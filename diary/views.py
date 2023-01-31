60559756a197f6cd52b101e60f490b15eb4dc0c9
Hozaki <hozaki.hs@gmail.com>
2023-01-31 14:15:11 +0900

    とりまコミット

--- diary/views.py
+++ diary/views.py
@@ -5,7 +5,7 @@ from django.urls import  reverse_lazy
 from django.views import generic
 from django.contrib import messages
 from .models import Diary
-from .forms import InquiryForm
+from .forms import InquiryForm,DiaryCreateForm
 
 logger = logging.getLogger(__name__)
 class IndexView(generic.TemplateView):
@@ -25,7 +25,32 @@ class InquiryView(generic.FormView):
 class DiaryListView(LoginRequiredMixin,generic.ListView):
     model = Diary
     template_name = 'diary_list.html'
-
+    paginate_by = 2
     def get_queryset(self):
-        diaries = Diary.objects.filter(user=self.request.user).order_by('-created_at')
+        diaries = Diary.objects.filter(user=self.requst.user).order_by('-created_at')
         return diaries
+
+class DiaryDetailView(LoginRequiredMixin,generic.DetailView):
+    model = Diary
+    template_name = 'diary_detail.html'
+
+class DiaryCreateView(LoginRequiredMixin,generic.CreateView):
+    model = Diary
+    template_name = 'diary_create.html'
+    form_class = DiaryCreateForm
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary=form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request,'日記作成しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request,"日記の作成に失敗しました。")
+        return  super().form_invalid(form)
+
+
+
+
